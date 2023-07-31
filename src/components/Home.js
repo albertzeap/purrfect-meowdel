b1d5745fd4ab5db4243fcb0ff691c0f34111256a
Albert <albertzeap@gmail.com>
2023-07-31 16:58:32 -0700

    Created basic header that also collapses on media devices

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import { CatCarousel } from "./Carousel"
+
+
+export const Home = () => {
+
+    return(
+        <div>
+            <h1>This is home</h1>
+            <CatCarousel/>
+        </div>
+    )
+}
---
+++ src/components/MeowNavBar.js
@@ -0,0 +1,40 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import logo from '../assets/logo.png'
+
+export const MeowNavBar = () => {
+  return (
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="/">
+                <img src={logo} height="100px" width="100px" alt='cat logo'/>
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Purrfect Menu
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Photos</Nav.Link>
+                  <Nav.Link href="#action2">Blog</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
