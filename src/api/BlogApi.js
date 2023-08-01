893b297904cc6e151dc2748383ea547c1e0b4bf9
Albert <albertzeap@gmail.com>
2023-08-01 16:29:27 -0700

    Linked Sanity.io headless CMS API to blog component

--- src/api/BlogApi.js
+++ src/api/BlogApi.js
@@ -1,6 +1,28 @@
 let PROJECT_ID = "ngvsxill";
 let DATASET = "production"
-let QUERY = encodeURIComponent('*[_type == "posts"]');
+let QUERY = encodeURIComponent('*[_type == "post"]');
 
-let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+const URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+const imageUrl  = "https://cdn.sanity.io/images/ngvsxill/production/"
 
+const BlogApi = {
+
+    getOrders:(setPostData) => {
+        fetch(URL)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            console.log(data.result);
+            setPostData(data.result);
+        })
+        .catch((error) => console.log(error));
+    },
+
+    getImageUrl:() => {
+        return imageUrl;
+    }
+
+}
+
+export default BlogApi;
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,53 +1,60 @@
 import {Col, Container, Row } from "react-bootstrap"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import BlogApi from "../api/BlogApi";
 
-
-const blogPosts = [
-    {
-      id: 1,
-      title: 'First Blog Post',
-      url: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1143&q=80",
-      content: 'This is the content of the first blog post.',
-      author: 'John Doe',
-      date: 'June 22, 2023'
-    },
-    {
-      id: 2,
-      title: 'Second Blog Post',
-      url: "https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80",
-      content: 'This is the content of the second blog post.',
-      author: 'Jane Smith',
-      date: 'June 23, 2023'
-    },
-    {
-      id: 3,
-      title: 'Second Blog Post',
-      url: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1143&q=80",
-      content: 'This is the content of the second blog post.',
-      author: 'Jane Smith',
-      date: 'June 23, 2023'
-    },
-    {
-      id: 4,
-      title: 'Second Blog Post',
-      url: "../assets/anbuLanding.jpg",
-      content: 'This is the content of the second blog post.',
-      author: 'Jane Smith',
-      date: 'June 23, 2023'
-    },
-    // Add more blog posts as needed
-  ];
-
+// const imageUrl  = "https://cdn.sanity.io/images/ngvsxill/production/"
 
 export const Blog = () => {
 
-    const[postData, setPostData] = useState(null);
+
+    const[postData, setPostData] = useState([]);
+    useEffect(()=> {
+     
+      BlogApi.getOrders(setPostData);
+      
+
+      // console.log(postData[0].title)
+
+    }, [])
 
     return(
     <Container className="mt-5">
       <h1 className="mb-4">Purrfect Blog</h1>
+      {postData.map((post) => {
+        {
+          console.log(post);
+          console.log("Title: " + post.title);
+          console.log("Id: " + post._id);
+          console.log("Content: " + post.body[0].children[0].text);
+          console.log("Image: " + BlogApi.getImageUrl() + post.mainImage.asset._ref.replace('image-', '').replace('-jpg', '.jpg'))
+
+        }
+        // <Col key={post._id}>
+
+        // </Col>
+        // <h1>{post.title}</h1>
+      })}
       <Row>
-        {blogPosts.map((post) => (
+
+
+        {postData.map((post) => (
+          <Col key={post._id} md={4}>
+            <div key={post._id} id="productCard" className="card text-start text-center m-3 align-content-center" style={{width: "18rem"}}>
+            <img className="card-img-top" src={BlogApi.getImageUrl() + post.mainImage.asset._ref.replace('image-', '').replace('-jpg', '.jpg')} alt="Title" style={{height: "15rem"}}/>
+            <div className="card-body">
+                <h4 id="productName" className="card-title" style={{color: "#3D2813"}}>{post.title}</h4>
+                <p className="card-text text-muted" style={{height: "5rem"}}>{post.body[0].children[0].text}</p>
+                <div className="card-footer" >
+                    <small className="text-muted">
+                        Posted on {post.publishedAt}
+                    </small>
+                    
+                </div>
+            </div>
+            </div>
+          </Col>
+        ))}
+        {/* {blogPosts.map((post) => (
           <Col key={post.id} md={4}>
             <div key={post.id} id="productCard" className="card text-start text-center m-3 align-content-center" style={{width: "18rem"}}>
             <img className="card-img-top" src={post.url} alt="Title" style={{height: "15rem"}}/>
@@ -63,7 +70,7 @@ export const Blog = () => {
             </div>
             </div>
           </Col>
-        ))}
+        ))} */}
       </Row>
     </Container>
     )
